f7f9375ecaa4af2bb9109af0431751eb90631e45
Lars Veldscholte <lars@tuxplace.nl>
2019-07-16 11:08:54 +0200

    Refactoring, documentation, and fixes

--- run_rcb.py
+++ run_rcb.py
@@ -2,11 +2,17 @@
 
 """
 Refines grid using pairwise recursive bisectioning (RCB) sampling
+
+The pairs come from the results of the grid sampling; they are pairs of points that cross a regime boundary.
+Bisectioning will occur between the pairs until the distance is less than RCB_THRESHOLD.
+Every bisectioning job is run in parallel using Python multiprocessing and starts in bisection_job() with an
+equilibration and long initial GCMC run of 9M timesteps. bisection_job() then calls recursive_bisect() to start the real
+bisectioning; the first iteration will not be simulated because it was the inital run.
 """
 
 from multiprocessing import Pool
 from os import mkdir, chdir, path
-from typing import Tuple, List
+from typing import Tuple, List, Callable
 
 from lammps import lammps
 import numpy as np
@@ -28,36 +34,35 @@ pairs: List[Tuple[Tuple[float, float], Tuple[float, float]]] = [
 ]
 
 
-def sample_coord(lmp, epp: float, eps: float) -> int:
+def sample_coord(lmp: lammps, epp: float, eps: float) -> int:
 	"""
 	Simulate a system with the given parameters and analyse the results to return a regime classification.
 	:param lmp: LAMMPS instance
 	:param epp: self-interation of polymer
 	:param eps: interaction of polymer with solvent
-	:return:
+	:return:    Classification
 	"""
-	# Create a subdirectory for every simulation. Skip simulation entirely if dir already exists
-	subdir = 'rcb_{}_{}'.format(epp, eps)
-	if not path.isdir(subdir):
-		mkdir(subdir)
-
-		chdir(subdir)
+	def sim():
 		lmp.command('variable epp equal {}'.format(epp))
 		lmp.command('variable eps equal {}'.format(eps))
-		lmp.file('../in.b_gcmc_run')
-		chdir('../')
+		# GCMC run (continuing, 6M timesteps)
+		lmp.file('../in.b_gcmc_rcb')
+		lmp.command('run 6000000')
+		lmp.command('write_data	data.gcmc')
+
+	subdir = 'rcb_{}_{}'.format(epp, eps)
+	run_in_subdir(sim, subdir)
 
 	rc = RegimeClassifier(subdir)
 	return rc.get_classification()
 
 
-def recursive_bisect(lmp, l: Tuple[float, float], r: Tuple[float, float]):
+def recursive_bisect(lmp, l: Tuple[float, float], r: Tuple[float, float]) -> None:
 	"""
 	Returns the middlepoint between two coords
 	:param lmp: LAMMPS instance
 	:param l:   Left coord
 	:param r:   Right coord
-	:return:
 	"""
 	if np.linalg.norm(np.array(l) - np.array(r)) > RCB_THRESHOLD:
 		# Calculate midpoint between two bounds
@@ -74,16 +79,15 @@ def recursive_bisect(lmp, l: Tuple[float, float], r: Tuple[float, float]):
 			# We're to the right, make current coord the right bound and recurse
 			return recursive_bisect(lmp, l, midpoint)
 		else:
-			raise RuntimeError("This should not happen")
+			raise RuntimeError("This should not happen. Classification = {}".format(classification))
 
 
-def bisection_job(l: Tuple[float, float], r: Tuple[float, float]):
+def bisection_job(l: Tuple[float, float], r: Tuple[float, float]) -> None:
 	"""
-	Instantiates a LAMMPS object and runs equilibration.
-	Wraps recursive_bisect().
+	Instantiates a LAMMPS object and runs equilibration and initial GCMC run.
+	Then calls recursive_bisect().
 	:param l: Left coord
 	:param r: Right coord
-	:return:
 	"""
 	# Calculate midpoint between two bounds for equilibration
 	midpoint = tuple((np.array(l) + np.array(r)) / 2)
@@ -91,18 +95,38 @@ def bisection_job(l: Tuple[float, float], r: Tuple[float, float]):
 	# Instantiate LAMMPS object
 	lmp = lammps()
 
-	lmp.command('variable epp equal {}'.format(midpoint[0]))
-	lmp.command('variable eps equal {}'.format(midpoint[1]))
+	epp, eps = midpoint
 
-	# Run 'header' of equilibration input file
-	lmp.file('../in.b_equi_header')
-	# Equilibrate
-	lmp.file('../in.b_equi_run')
+	def sim():
+		lmp.command('variable epp equal {}'.format(epp))
+		lmp.command('variable eps equal {}'.format(eps))
+		# Run 'header' of equilibration input file
+		lmp.file('../in.b_equi_header')
+		# Equilibrate
+		lmp.file('../in.b_equi_run')
+		# Initial GCMC run (from scratch, 9M timesteps)
+		lmp.file('../in.b_gcmc_rcb')
+		lmp.command('run 9000000')
+		lmp.command('write_data	data.gcmc')
+
+	run_in_subdir(sim, 'rcb_{}_{}'.format(epp, eps))
 
 	# Start recursive bisectioning
 	recursive_bisect(lmp, l, r)
 
 	lmp.close()
+
+
+def run_in_subdir(func: Callable[[], None], subdir: str) -> None:
+	"""
+	Runs a simulation in a subdir.
+	"""
+	# Create a subdirectory for every simulation. Skip simulation entirely if dir already exists
+	if not path.isdir(subdir):
+		mkdir(subdir)
+		chdir(subdir)
+		func()
+		chdir('../')
 
 
 if __name__ == '__main__':
