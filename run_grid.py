a5e8702bc38a9eee8c19ec44ff283ff0a793dc96
Lars Veldscholte <lars@tuxplace.nl>
2019-08-13 13:50:40 +0200

    Clearify help messages

--- run_grid.py
+++ run_grid.py
@@ -63,10 +63,13 @@ def run_in_subdir(input_script: str, subdir: str) -> None:
 if __name__ == '__main__':
 	# Parse CLI arguments
 	parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument("epp", type=lambda s: [float(i) for i in s.split(',')], help="Comma-separated list of EPP values")
-	parser.add_argument("eps", type=lambda s: [float(i) for i in s.split(',')], help="Comma-separated list of EPS values")
+	parser.add_argument("epp", type=lambda s: [float(i) for i in s.split(',')],
+	                    help="Comma-separated list of Epp values")
+	parser.add_argument("eps", type=lambda s: [float(i) for i in s.split(',')],
+	                    help="Comma-separated list of Eps values")
 	parser.add_argument("--dry-run", action="store_true", help="Don't actually do anything productive.")
-	parser.add_argument("-l", "--lammps-path", default='lmp_daily', help="Path to the LAMMPS binary")
+	parser.add_argument("-l", "--lammps-path", default='lmp_daily',
+	                    help="Path to the LAMMPS binary. May be a command, like \"mpirun lmp\"")
 	parser.add_argument("-r", "--run", type=int, default=9000000, help="Run GCMC for n timesteps")
 	args = parser.parse_args()
 
