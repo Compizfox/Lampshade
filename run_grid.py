41f1a15b66d37ce96588a04138483f49c1c87699
lars <lars@tuxplace.nl>
2020-02-25 16:29:00 +0100

    Overhaul wrapper:
    - Remove run_single.py (redundant)
    - Pass equal-style variables to LAMMPS using command-line arguments instead of the StringIO input file builder approach
    - Add settings.ini file as a method to pass more settings (e.g. MPI/LAMMPS paths/arguments) instead of them being hardcoded, as well as a method to pass LAMMPS variables instead of via the command line
    - Assorted refactoring and documentation/comment improvements

--- run_grid.py
+++ run_grid.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
 """
-Samples a mesh grid in the 2D EPP-EPS parameter space.
-No script-level MPI; different points are simulated in parallel using Python multiprocessing.
+Runs a range of simulation jobs with arbitrarily varied parameters.
+No script-level MPI; different jobs are simulated in parallel using Python multiprocessing.
+
+Maximum number of cores, paths to LAMMPS and mpirun and extra arguments to those are set in settings.ini.
 
 Runs a (single, common) equilibration if equilibrated data file is not found with n-rank MPI where n is the number of
 cores in the system.
 
-By default, calls LAMMPS with 1x OMP and auto-calculated MPI ranks to saturate the cores in te system.
+Variables for the equilibration run must be set in settings.ini. Variables for the GCMC run can be set either in
+settings.ini (static), or passed through the -var/--variable command-line argument, in which case an array of values
+can be specified which will be run in parallel.
 
-Requires psutil.
+Fills up 'spare' cores with MPI parallelisation.
 """
 
-from multiprocessing import Pool, current_process, cpu_count
-from itertools import product
-from os import path
-from math import floor
 import argparse
-
-import psutil
+import configparser
+from itertools import product
+from math import floor
+from multiprocessing import Pool, current_process
+from os import path
+from typing import Optional
 
 from Simulation import Simulation
 
@@ -27,23 +31,44 @@ from Simulation import Simulation
 # callable by Pool.starmap for some reason
 def start_sim(*vars_values: float) -> None:
 	"""
-	Start the simulation by creating a Simulation and running it
+	Start the simulation by creating a Simulation and running it.
 	:param float *vars_values: Custom variable values
 	"""
-	# Reconstruct vars dict
-	vars = {k: v for k, v in zip(args.variable.keys(), vars_values)}
+	static_vars = dict(config.items('gcmc_vars'))
 
-	sim = Simulation(args.lammps_path, args.dry_run, current_process().name)
-	sim.sample_coord(vars)
+	# Reconstruct dynamic vars dict
+	dyn_vars = dict(zip(args.variable.keys(), vars_values))
+
+	sim = Simulation(args.lammps_command, args.dry_run, current_process().name)
+	sim.run_gcmc(static_vars, dyn_vars)
 
 
-def init_pool(args_dict: dict) -> None:
+def init_pool(args_dict: dict, config_dict: dict) -> None:
 	"""
-	Initialise workers with arguments dict as global args
+	Initialise workers with arguments dict and config dict as global args.
 	:param dict args_dict: Arguments
+	:param dict config_dict: Config
 	"""
-	global args
+	global args, config
 	args = args_dict
+	config = config_dict
+
+
+def build_lammps_command(lammps_path: str, lammps_arguments: str, mpi_path: Optional[str], mpi_arguments: Optional[str],
+                         mpi_ranks: Optional[int]) -> str:
+	"""
+	Build LAMMPS command from paths and arguments, optionally with MPI.
+	:param str lammps_path:      Path to LAMMPS
+	:param str lammps_arguments: Arguments to LAMMPS
+	:param str mpi_path:         Path to mpirun (optional)
+	:param str mpi_arguments:    Arguments to mpirun (mandatory if mpi_path is given)
+	:param int mpi_ranks:        Number of MPI ranks to use (mandatory if mpi_path is given)
+	:return: String containing the assembled LAMMPS command
+	"""
+	if mpi_path is not None:
+		return ' '.join([mpi_path, mpi_arguments, '-n', str(mpi_ranks), lammps_path, lammps_arguments])
+	else:
+		return ' '.join([lammps_path, lammps_arguments])
 
 
 if __name__ == '__main__':
@@ -60,35 +85,50 @@ if __name__ == '__main__':
 	                         "Multiple variables should be separated by spaces. Values should be floats. Example: -var "
 	                         "mu=1.0,2.0 cps=1.5,2.0",
 	                    metavar="VAR=VALUE")
-
 	parser.add_argument("--dry-run", action="store_true", help="Don't actually do anything productive.")
-	parser.add_argument("-l", "--lammps-path", default='lmp',
-	                    help="Path to the LAMMPS binary. May be a command, like \"mpirun lmp\"")
 	args = parser.parse_args()
 
-	# Create cartesian product of parameters. Returns list of tuples (*vars)
-	coordList = list(product(*args.variable.values()))
+	# Parse ini file
+	config = configparser.ConfigParser(converters={'list': lambda s: s.split(' ')})
+	config.optionxform = str  # Use case-sensitive keys
+	config.read('settings.ini')
 
-	cpu_count = psutil.cpu_count(logical=False)
-
-	print("Got {} jobs, distributed over {} workers:".format(len(coordList), cpu_count))
-	print(*coordList, sep="\n")
+	cpu_count = int(config['system']['num_cores'])
+	lammps_path = config['lammps']['LAMMPS_path']
+	lammps_arguments = config['lammps'].get('LAMMPS_arguments', '')
+	mpi_path = config['lammps'].get('MPI_path')
+	mpi_arguments = config['lammps'].get('MPI_arguments', '')
 
 	# Run equilibration if equilibrated data file is not found
 	if not path.isfile('data.equi'):
-		if args.lammps_path == 'lmp':
-			args.lammps_path = 'mpirun -np {} lmp -sf omp -pk omp 1'.format(cpu_count)
+		# Gather equi vars from config
+		equi_vars = dict(config.items('equi_vars'))
 
-		# Get first values of vars
-		first_vars = {k: v[0] for k, v in args.variable.items()}
+		# Assert all required equi vars are accounted for
+		for var in config.getlist('job', 'required_equi_vars'):
+			if var not in equi_vars:
+				raise RuntimeError("Missing value for equilibration variable '{}'".format(var))
 
-		sim = Simulation(args.lammps_path, args.dry_run)
-		sim.run_equi(first_vars)
+		lammps_command = build_lammps_command(lammps_path, lammps_arguments, mpi_path, mpi_arguments, cpu_count)
+		sim = Simulation(lammps_command, args.dry_run)
+		sim.run_equi(equi_vars)
 
-	if args.lammps_path == 'lmp':
-		mpi_ranks = floor(cpu_count / len(coordList))
-		args.lammps_path = 'mpirun --bind-to socket -np {} lmp -sf omp -pk omp 1'.format(mpi_ranks)
+	# Assert all required gcmc vars are accounted for
+	for var in config.getlist('job', 'required_gcmc_vars'):
+		if var not in config['gcmc_vars'] and var not in args.variable.keys():
+			raise RuntimeError("Missing value for GCMC variable '{}'".format(var))
+
+	# Create Cartesian product of parameters. Returns list of tuples (*vars)
+	dyn_vars_list = list(product(*args.variable.values()))
+
+	print("Got {} jobs, distributed over {} workers:".format(len(dyn_vars_list), cpu_count))
+	print(*dyn_vars_list, sep="\n")
+
+	# Calculate number of 'spare' cpu cores for MPI parallelisation
+	mpi_ranks = floor(cpu_count / len(dyn_vars_list))
+	if mpi_ranks == 0: mpi_ranks = 1
+	args.lammps_command = build_lammps_command(lammps_path, lammps_arguments, mpi_path, mpi_arguments, mpi_ranks)
 
 	# Create pool of number of cores workers
-	with Pool(cpu_count, initializer=init_pool, initargs=(args,)) as p:
-		p.starmap(start_sim, coordList)
+	with Pool(cpu_count, initializer=init_pool, initargs=(args, config)) as p:
+		p.starmap(start_sim, dyn_vars_list)
